7bb11cead7ba48802043923e10ec19c60c53b7f6
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-03-04 18:47:26 -0300

    Pega a localização e devolve o clima

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-aaaaaaaaaaaaaaaaa
+
+
+
+
+function pegarClima(latitude, longitude) {
+    axios.post(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=pt_br&units=metric&appid=f25110b0f83adb9f7c080ee182cd1d00`
+    ).then(response => {
+        console.log(response);
+    });
+}
+
+function pegarLocalizacao() {
+    navigator.geolocation.getCurrentPosition(position => {
+        pegarClima(position.coords.latitude, position.coords.longitude);
+    });
+}
+pegarLocalizacao();
